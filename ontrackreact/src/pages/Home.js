9bf20a418e90a3f867abb1eddfadeb3dcad58521
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-10-19 23:33:40 -0500

    NextTesting

---
+++ ontrackreact/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>OnTrack</h1>
+        <button>Start Session</button>
+        <p>Session Time: 0:00</p>
+    </div>
+  )
+}
+
+export default Home
